350344394b1527a91a4a5bf54a598bfd42317923
iSalah-YT <kurditstation@gmail.com>
2022-05-22 17:24:55 +0100

    welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import ImageCard from './components/ImageCard';
-import { Container, Row, Col } from 'react-bootstrap';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+import Welcome from "./components/Welcome";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
@@ -23,7 +24,7 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
-    setWord('');
+    setWord("");
   };
 
   const handleDeleteImage = (id) => {
@@ -34,14 +35,18 @@ const App = () => {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <Container className='mt-4'>
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            <Col key={i} className='pb-3'>
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-          ))}
-        </Row>
+      <Container className="mt-4">
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col key={i} className="pb-3">
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
       </Container>
     </div>
   );
